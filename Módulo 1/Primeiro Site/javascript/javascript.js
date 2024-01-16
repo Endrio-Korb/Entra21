c95630d4f890338a5f81bcd0223010f1b92e9baf
Endrio-Korb <alexkorb95@gmail.com>
2024-01-16 11:01:04 -0300

    primeiro commit

---
+++ Módulo 1/Primeiro Site/javascript/javascript.js
@@ -0,0 +1,16 @@
+var nome = "Endrio";
+var numero = 22;
+var letra = "E";
+var exemploDecimal = 40.87653;
+
+console.log(nome, numero, letra, exemploDecimal);
+
+
+function soma_numeros() {
+    var a = 5;
+    var b = 5;
+    var resultado = a + b;
+    console.log(resultado);
+}
+
+soma_numeros();
